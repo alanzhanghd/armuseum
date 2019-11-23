aadecb002f70338d11724b582454adad29637f44
Eric Wang <erwang01@gmail.com>
2019-11-22 17:24:42 -0800

    added MRsive minimap code

---
+++ Assets/MRsive/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿//Code controlling the top view map
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(Target.position.x, Target.positiony, Target.position.z);
+        transform.rotation = Quaternion.Euler(90f, Target.eulerAngles.y, 0f);
+    }
+}
---
+++ Assets/MRsive/README.md
@@ -0,0 +1,2 @@
+The files in this folder is copied from Jad Al Rabbaa's Thesis.
+http://openresearch.ocadu.ca/id/eprint/2496/35/AlRabbaa_Jad_2019_MDES_DF_Thesis.pdf
